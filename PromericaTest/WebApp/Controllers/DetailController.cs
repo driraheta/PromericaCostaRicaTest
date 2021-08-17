2a457ce9c47afd38d7c195c1b43450d8f4ecb6f2
Diego_Iraheta <diego.iraheta77@gmail.com>
2021-08-17 16:50:57 -0600

    Agrego frontend, vista maestro, vista detalle, comunicacion con API.

---
+++ PromericaTest/WebApp/Controllers/DetailController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace WebApp.Controllers
+{
+    public class DetailController : Controller
+    {
+        private readonly ILogger<DetailController> _logger;
+        public IConfiguration Configuration { get; }
+
+        public DetailController(ILogger<DetailController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            string endpoint = "Patterns/" + id.ToString();
+            List<Pattern> patterns = new List<Pattern>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Configuration.GetValue<string>("api") + endpoint))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    patterns = JsonConvert.DeserializeObject<List<Pattern>>(apiResponse);
+                }
+            }
+
+            return View(patterns);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PromericaTest/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string endpoint = "Makes";
+            List<Make> makes = new List<Make>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Configuration.GetValue<string>("api") + endpoint))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    makes = JsonConvert.DeserializeObject<List<Make>>(apiResponse);
+                }
+            }
+            return View(makes);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
